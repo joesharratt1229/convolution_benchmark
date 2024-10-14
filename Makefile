cd5be453c6611a4a075f3203577c2999c7d36b07
joesharratt1229 <joesharratt1229@gmail.com>
2024-10-14 17:40:18 +0000

    added thread coarsening

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ CPP=g++
 NVCC=nvcc
 CFLAGS=--std=c++11 -g --default-stream per-thread
 DEBUGFLAGS=-G -lineinfo
+REGFLAGS=-maxrregcount=30
 
 MODULE := conv2b
 
@@ -13,10 +14,10 @@ debug: CFLAGS += $(DEBUGFLAGS)
 debug: $(MODULE)
 
 $(MODULE): convolution.cu $(HEADERS)
-	$(NVCC) $^ $(CFLAGS) -o $@ -DNx=2250 -DNy=1250 -DKx=7 -DKy=7 -DNi=3 -DNn=768
+	$(NVCC) $^ $(CFLAGS) $(REGFLAGS) -o $@ -DNx=2250 -DNy=1250 -DKx=7 -DKy=7 -DNi=3 -DNn=32
 
 debug_build: convolution.cu $(HEADERS)
-	$(NVCC) $^ $(CFLAGS) -o $@ -DNx=2250 -DNy=1250 -DKx=7 -DKy=7 -DNi=3 -DNn=768 -G -lineinfo
+	$(NVCC) $^ $(CFLAGS) $(REGFLAGS) -o $@ -DNx=2250 -DNy=1250 -DKx=7 -DKy=7 -DNi=3 -DNn=768 -G -lineinfo
 
 clean:
 	rm -f $(MODULE)
--- convolution.cu
+++ convolution.cu
@@ -39,12 +39,13 @@ __host__ void convolution_cpu(float h_input[Ni][NyPad][NxPad], float h_filters[N
 __host__ void checkOutput(float h_output[Nn][Oy][Ox], float h_output_cpu[Nn][Oy][Ox]);
 
 
-
 int main(int argc, char **argv) {
     bool DEBUG = ((argc > 1) && (std::string(argv[1]) == "--debug"));
 
+    unsigned int Ox2 = (Ox + 1) / 2;
+
     dim3 threadsPerBlock(TILE_SIZE, TILE_SIZE, CHANNEL_SIZE);
-    dim3 blocksPerGrid((Ox + threadsPerBlock.x - 1) / threadsPerBlock.x,
+    dim3 blocksPerGrid((Ox2 + threadsPerBlock.x - 1) / threadsPerBlock.x,
                        (Oy + threadsPerBlock.y - 1) / threadsPerBlock.y,
                        (Nn + threadsPerBlock.z - 1) / threadsPerBlock.z);
 
@@ -98,34 +99,54 @@ int main(int argc, char **argv) {
 
 __global__
 void conv_2d(float d_input[Ni][NyPad][NxPad], float d_filters[Nn][Ni][Ky][Kx], float d_output[Nn][Oy][Ox]) {
-    unsigned int col = blockIdx.x * TILE_SIZE + threadIdx.x;
+    unsigned int col = 2*(blockIdx.x * TILE_SIZE + threadIdx.x);
     unsigned int row = blockIdx.y * TILE_SIZE + threadIdx.y;
     unsigned int output_channel = blockIdx.z * CHANNEL_SIZE + threadIdx.z;
 
-    __shared__ float input_cache[Ni][INPUT_TILE_Y][INPUT_TILE_X];
+    __shared__ float input_cache[Ni][INPUT_TILE_Y][INPUT_TILE_X*2];
 
-    if (threadIdx.x == 0 && threadIdx.y == 0 && threadIdx.z == 0) {
-        for (int i = 0; i < Ni; i++)
-            for (int y = 0; y < INPUT_TILE_Y; y++)
-                for (int x = 0; x < INPUT_TILE_X; x++) {
-                    input_cache[i][y][x] = d_input[i][row*StrideY + y][col*StrideX + x];
-                }
-    } 
+    if (threadIdx.z < Ni && StrideY * threadIdx.y < INPUT_TILE_Y && StrideX * threadIdx.x < INPUT_TILE_X) {
+        if (threadIdx.y < TILE_SIZE - 1)
+            for (int y = 0; y < StrideY; y++)
+                if (threadIdx.x < TILE_SIZE - 1)
+                    for (int x = 0; x < 2* StrideX; x++) 
+                        input_cache[threadIdx.z][StrideY * threadIdx.y + y][2*StrideX * threadIdx.x + x] = d_input[threadIdx.z][row*StrideY + y][col*StrideX + x];
+                else
+                    for (int x = 0; x < 2* StrideX + Kx - 1; x++) 
+                        input_cache[threadIdx.z][StrideY * threadIdx.y + y][2*StrideX * threadIdx.x + x] = d_input[threadIdx.z][row*StrideY + y][col*StrideX + x];
+        else 
+            for (int y = 0; y < StrideY + Ky - 1; y++)
+                if (threadIdx.x < TILE_SIZE - 1)
+                    for (int x = 0; x < 2* StrideX; x++) 
+                        input_cache[threadIdx.z][StrideY * threadIdx.y + y][2*StrideX * threadIdx.x + x] = d_input[threadIdx.z][row*StrideY + y][col*StrideX + x];
+                else
+                    for (int x = 0; x < 2* StrideX + Kx - 1; x++) 
+                        input_cache[threadIdx.z][StrideY * threadIdx.y + y][2*StrideX * threadIdx.x + x] = d_input[threadIdx.z][row*StrideY + y][col*StrideX + x];
+    }
 
     __syncthreads();
 
-    float sum = 0.0f;
+    float sum1 = 0.0f;
+    float sum2 = 0.0f;
 
-    if (row < Oy && col < Ox && output_channel < Nn) {
+    if (row < Oy && output_channel < Nn) {
         #pragma unroll
         for (int i = 0; i < Ni; i++)
             #pragma unroll
             for (int y = 0; y < Ky; y++)
                 #pragma unroll
                 for (int x = 0; x < Kx; x++) {
-                    sum += input_cache[i][threadIdx.y * StrideY + y][threadIdx.x * StrideX + x] * d_filters[output_channel][i][y][x];
+                    float filter_val = d_filters[output_channel][i][y][x];
+                    sum1 += input_cache[i][threadIdx.y * StrideY + y][(2*threadIdx.x) * StrideX + x] * filter_val;
+                    sum2 += input_cache[i][threadIdx.y * StrideY + y][(2*threadIdx.x+1) * StrideX + x] * filter_val;
                 }
-        d_output[output_channel][row][col] = sum;
+        
+        if (col < Ox) {
+            d_output[output_channel][row][col] = sum1;
+        }
+        if (col+1 < Ox) {
+            d_output[output_channel][row][col+1] = sum2;
+        }
     }
 }
 
