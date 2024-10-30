7c0c7e3edc68fb2965a200f74625cd708c9d3830
joesharratt1229 <joesharratt1229@gmail.com>
2024-10-30 16:31:49 +0000

    added profiling commands

--- profile.sh
+++ profile.sh
@@ -1,1 +1,5 @@
-sudo ncu -f --set detailed --export report.nsight-cuprof-report ./conv2b
+
+sudo ncu -f --set full --export report.ncu-rep ./conv2b
+sudo ncu -f --set detailed --export report.ncu-rep ./conv2b
+sudo ncu --section SpeedOfLight --kernel-name pos_embedding_kernel --launch-skip 1 --launch-count 1 ./conv2b
+sudo ncu -c 3 --section SpeedOfLight ./conv2b 
