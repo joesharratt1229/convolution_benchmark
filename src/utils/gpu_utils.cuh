7238b18d031319f0c9004f64be7866445aded18e
joesharratt1229 <joesharratt1229@gmail.com>
2024-10-31 17:54:29 +0000

    added block config

--- src/utils/gpu_utils.cuh
+++ src/utils/gpu_utils.cuh
@@ -10,4 +10,17 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort =
     }
 }
 
+namespace cuda_config {
+    // For convolution kernel (2D spatial + channel)
+    struct StandardConfig {
+        static constexpr int BLOCK_X = 32;  // Spatial X dimension
+        static constexpr int BLOCK_Y = 32;  // Spatial Y dimension
+        static constexpr int BLOCK_Z = 1;   // Channel dimension
+        
+        static dim3 block_dim() { 
+            return dim3(BLOCK_X, BLOCK_Y, BLOCK_Z); 
+        }
+    };
+}
+
 #endif
