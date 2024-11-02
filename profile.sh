427fc88acb8ed2f365910b60efd8db7fd574d98c
joesharratt1229 <joesharratt1229@gmail.com>
2024-11-02 13:30:48 +0000

    added profile tool

--- profile.sh
+++ profile.sh
@@ -3,3 +3,5 @@ sudo ncu -f --set full --export report.ncu-rep ./conv2b
 sudo ncu -f --set detailed --export report.ncu-rep ./conv2b
 sudo ncu --section SpeedOfLight --kernel-name pos_embedding_kernel --launch-skip 1 --launch-count 1 ./conv2b
 sudo ncu -c 3 --section SpeedOfLight ./conv2b 
+
+sudo ncu -k conv_and_bilinear_resid_kernel ./conv2b 
